a09ad446c5e226a00370f91b3200029c06084efb
Bogdan Florea <bogdanf@Joaos-MacBook-Pro.local>
2019-05-26 02:13:33 +0300

    version 1

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
+
+app.get('/current_version', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
+    response.json({"version": "v1"});
+});
+
 app.get('/cities', (request, response) => {
     response.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
     response.json(require("./"+request.query.version+"/"+request.query.country+"/citiesMin.json"));
